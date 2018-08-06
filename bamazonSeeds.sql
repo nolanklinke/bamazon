9eed6ef8e3e604a18dcd5d1ec5ed6bed3b6b92c3
disconaked <nolan.klinke@gmail.com>
2018-08-06 09:43:22 -0400

    initial commit

---
+++ bamazonSeeds.sql
@@ -0,0 +1,17 @@
+-- ### Alternative way to insert more than one row
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+("Computer", "Electronics", 999.99, 2),
+("Hairbrush", "Beauty", 8.99, 50),
+("Candy Bar", "Food", .99, 30),
+("Milk", "Food", 3.99, 25),
+("Shoes", "Clothing", 99.99, 10),
+("Face Masks", "Beauty", 9.99, 100),
+("Headphones", "Electronics", 24.99, 25),
+("Vacuum", "Household Items", 299.99, 30),
+("Couch", "Furniture", 499.99, 5),
+("Video Game", "Electronics", 69.99, 15),
+
