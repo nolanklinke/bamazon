7efb5ae8ad7a2b5dc8929a94b0db5c381905b1b1
disconaked <nolan.klinke@gmail.com>
2018-08-07 15:51:08 -0400

    almost

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -35,7 +35,7 @@ function askUser () {
         {
         name: "openStore",
         type: "input",
-        message: "\nPlease insert the product ID of the product you would like to buy.",
+        message: "\nPlease insert the product ID of the item you would like to buy.",
         validate: function(value) {
             if (isNaN(value) === false) {
               return true;
@@ -46,24 +46,52 @@ function askUser () {
 ]).then(function(answer) {
     connection.query("SELECT * FROM products WHERE item_id=?", [answer.openStore], function(err, res) {
         if (err) throw err;
-        console.log(`You have selected to purchase the following product: ${res[0].product_name}.`);
-        confirmChoice();
+        console.log(`You have selected to purchase the following item: ${res[0].product_name}.\n`);
+        console.log(`There are ${res[0].stock_quantity} of this item remaining.`);
+        //confirmChoice();
+        inquirer.prompt([{
+            name: "howMany",
+            type: "input",
+            message: "How many would you like to purchase?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+            }
+        ]).then(function(answer) {
+            console.log(`You have chosen to purchase ${answer.howMany}.`);
+
+            if (answer.howMany <= res[0].stock_quantity) {
+                console.log(`You have successfully purchased ${answer.howMany} ${res[0].product_name}.\n`);
+                console.log(`Thank you for your purchase, have a nice day!`);
+                //connection.query(`UPDATE products SET ${res[0].stock_quantity} = ${res[0].stock_quantity} - ${answer.howMany} WHERE ${res[0].product_name}`);
+                console.log(`There are now ${res[0].stock_quantity} left in stock.`)
+                connection.end();
+            } else {
+                console.log('Insufficient Quantity!')
+                connection.end();
+            }
+        })
     })
 })
 }
 
-function confirmChoice () {
-    inquirer.prompt([{
-        name: "confirm",
-        type: "confirm",
-        message: "Are you sure?"
-    }
-]).then(function(answer) {
-   if (answer.confirm == true) {
-       console.log("Yes")
-   } else {
-       console.log("No")
-   }
-   connection.end();
-})
-}
+
+
+//function confirmChoice () {
+//    inquirer.prompt([{
+//        name: "confirm",
+//        type: "confirm",
+//        message: "Are you sure?"
+//    }
+//]).then(function(answer) {
+//   if (answer.confirm == true) {
+//       console.log("Yes")
+//   } else {
+//       console.log("No")
+//   }
+//   connection.end();
+//})
+//}
